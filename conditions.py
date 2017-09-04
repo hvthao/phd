5726cfce2add8f5b8ff741a352fbf31135cfb02e
hvthao <hvthao@yahoo.com>
2017-09-04 10:41:18 +0700

    update from ubuntu.

---
+++ conditions.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+
+name = raw_input("Name: ")
+age = input('Age: ')
+
+if age > 20:
+    print(name, 'you are older than 20 years old!')
+else:
+    print(name, 'you are younger than 20.')
---
+++ io.py
@@ -0,0 +1,4 @@
+from __future__ import print_function
+
+userName = raw_input('Please enter your name: ')
+print("Hello", userName + '!')
